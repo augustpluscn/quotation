2d0d9fee6cf51a8509fd7b62e0c179093d764ad3
august <1@augustor.com>
2021-12-02 17:28:47 +0800

    修改标题

--- resources/js/vue.config.js
+++ resources/js/vue.config.js
@@ -3,4 +3,12 @@ module.exports = {
   indexPath: process.env.NODE_ENV === 'production'
     ? '../resources/views/app.blade.php'
     : 'index.html',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '报价'
+        return args
+      })
+  }
 };
