cf4cda8582604866d515f991fe3540d2266a6ea3
august <1@augustor.com>
2021-12-02 11:29:36 +0800

    封装输入框

---
+++ resources/js/src/components/Number.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="number">
+    <van-field :required='required' v-model="val" type="number" :label="lavel" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Number",
+  props: {
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    lavel: {
+      type: String,
+      default: "标题",
+    },
+    val: 0,
+  },
+  computed: {},
+  methods: {},
+};
+</script>
